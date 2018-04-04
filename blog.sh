e4703bfdaecbe804952405d39ff7ba7109d6cbd0
Richard van Roy <pluizer@freeshell.de>
2018-04-05 00:38:12 +0200

    initial

---
+++ blog.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# all articles by date
+articles=`ls -t articles/*.md`
+
+# return the title an article
+function title() {
+    head -n 1 $1 | cut -c 3-
+}
+
+# place to header of the page
+cat pre.html
+
+echo '<div id="wrapper">'
+
+# place links to articles
+echo "<nav><ul>"
+for filename in $articles; do
+    title=`title $filename`
+    echo "  <li><a href=\"#${title// /-}\">$title</a></li>"
+done;
+echo "</ul></nav>"
+
+# place articles
+for filename in $articles; do
+    title=`title $filename`
+    echo "<a name=\"${title// /-}\"></a>"
+    echo "<section><article>"
+    markdown $filename
+    echo "</article></section>"
+done;
+
+echo "</div>"
+
+# place the footer of the page
+cat post.html
